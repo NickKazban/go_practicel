09e1d7490cc6214a061f2efcea643c5cf55a7441
NickKazban <mykola.kazban@gmail.com>
2025-07-29 14:56:28 +0300

    11-14

--- main-2.go
+++ main-2.go
@@ -77,3 +77,23 @@ func main() {
 	}
 }*/
 
+//////11-14////
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
