9605cb15b4f9baa1a8fc4acc2658b1d044319495
NickKazban <mykola.kazban@gmail.com>
2025-07-29 16:36:40 +0300

    1-27 task

--- start-section2.go
+++ start-section2.go
@@ -6,11 +6,11 @@ func main() {
 	i, j := 42, 2701
 
 	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
+	fmt.Println(*p) // read i through the pointer- 42
 	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
+	fmt.Println(i)  // see the new value of i. - 21 нове значення через показчик "і"
 
 	p = &j         // point to j
 	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+	fmt.Println(j) // see the new value of j - 73
 }
