54043e105f5dd5eba244f5eb89609e9aed2efadd
NickKazban <mykola.kazban@gmail.com>
2025-07-29 14:45:10 +0300

    task 10-14 and real example

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 ////9-14 ///////
 
-package main
+/*package main
 
 import (
 	"fmt"
@@ -20,3 +20,59 @@ func main() {
 		fmt.Printf("%s.\n", os)
 	}
 }
+*/
+
+////////10-14///
+
+/*package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+	fmt.Println(time.Now())    //Real time now
+
+}*/
+
+//// Різниця між днями (субота-сьогодні)/////
+
+/*package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+
+	// Вычисляем разницу между сегодняшним днем и субботой
+	daysUntilSaturday := (time.Saturday - today + 7) % 7
+
+	// Используем switch для вывода результата
+	switch daysUntilSaturday {
+	case 0:
+		fmt.Println("Today.")
+	case 1:
+		fmt.Println("Tomorrow.")
+	case 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Printf("Go to Saturday  to  %d days. Good luck man", daysUntilSaturday)
+	}
+}*/
