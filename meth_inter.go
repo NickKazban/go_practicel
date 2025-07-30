c884d09db0252ce646c67541d66024093b8b7751
NickKazban <mykola.kazban@gmail.com>
2025-07-30 14:29:27 +0300

    4-26 Pointer receivers/

--- meth_inter.go
+++ meth_inter.go
@@ -1,5 +1,48 @@
-// //11-26 Methods//
+// //1-26 Methods//
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// type Vertex struct {
+// 	X, Y float64
+// }
+
+// func (v Vertex) Abs() float64 {
+// 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	fmt.Println(v.Abs())			//можна і так 	fmt.Println(Abs(v))
+// }
+// //3-26 Methods continued//
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
+
+								// 4-26 Pointer receivers//
 package main
 
 import (
@@ -15,7 +58,13 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
 	v := Vertex{3, 4}
+	v.Scale(10)
 	fmt.Println(v.Abs())
 }
