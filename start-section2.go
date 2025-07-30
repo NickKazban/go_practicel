cbf7c4a06b1ec3bde3244dc455d9219855b5cbe2
NickKazban <mykola.kazban@gmail.com>
2025-07-30 14:02:56 +0300

    14-27 CSlices of slices

--- start-section2.go
+++ start-section2.go
@@ -246,25 +246,53 @@ func main() {
 // }
 // ////13-27 Creating a slice with make////
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	a := make([]int, 5)
+// 	printSlice("a", a)
+
+// 	b := make([]int, 0, 5)
+// 	printSlice("b", b)
+
+// 	c := b[:2]
+// 	printSlice("c", c)
+
+// 	d := c[2:5]
+// 	printSlice("d", d)
+// }
+
+//	func printSlice(s string, x []int) {
+//		fmt.Printf("%s len=%d cap=%d %v\n",
+//			s, len(x), cap(x), x)
+//	}
+//
+// //14-27 CSlices of slices///
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	a := make([]int, 5)
-	printSlice("a", a)
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
 
-	b := make([]int, 0, 5)
-	printSlice("b", b)
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "C"
+	board[1][2] = "K"
+	board[1][0] = "A"
+	board[0][2] = "B"
 
-	c := b[:2]
-	printSlice("c", c)
-
-	d := c[2:5]
-	printSlice("d", d)
-}
-
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n",
-		s, len(x), cap(x), x)
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 }
