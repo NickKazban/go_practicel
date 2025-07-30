2872d026b9b6c8a84560ef8ebc3af6e433d17979
NickKazban <mykola.kazban@gmail.com>
2025-07-30 12:02:01 +0300

    8-27 Slices are like references to arrays

--- start-section2.go
+++ start-section2.go
@@ -113,13 +113,36 @@ func main() {
 // }
 // ////7-27 Slices/////
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+//		var s []int = primes[1:4]
+//		fmt.Println(s)
+//	}
+//
+// ////8-27 Slices are like references to arrays////
 package main
 
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
 
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
