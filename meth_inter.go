f4bf276794ee92eaf4b0d81d3a7de94a6ad44e9d
NickKazban <mykola.kazban@gmail.com>
2025-07-31 11:57:04 +0300

    13-26

--- meth_inter.go
+++ meth_inter.go
@@ -99,32 +99,51 @@
 
 // 6-26 Methods and pointer indirection//
 
+// package main
+
+// import "fmt"
+
+// type Vertex struct {
+// 	X, Y float64
+// }
+
+// func (v *Vertex) Scale(f float64) {
+// 	v.X = v.X * f
+// 	v.Y = v.Y * f
+// }
+
+// func ScaleFunc(v *Vertex, f float64) {
+// 	v.X = v.X * f
+// 	v.Y = v.Y * f
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	v.Scale(2)
+// 	ScaleFunc(&v, 10)
+
+// 	p := &Vertex{4, 3}
+// 	p.Scale(3)
+// 	ScaleFunc(p, 8)
+
+// 	fmt.Println(v, p)		 //4*3*8//3*3*8
+// }
+
+// 13-26 Nil interface values//
 package main
 
 import "fmt"
 
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+type I interface {
+	M()
 }
 
 func main() {
-	v := Vertex{3, 4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
+	var i I
+	describe(i)
+	i.M()
+}
 
-	p := &Vertex{4, 3}
-	p.Scale(3)
-	ScaleFunc(p, 8)
-
-	fmt.Println(v, p)
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
