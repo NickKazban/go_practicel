406fb9e45dcfb12716fdb62bac6c7cf43f759e08
NickKazban <mykola.kazban@gmail.com>
2025-07-31 12:36:12 +0300

    Finish course

--- meth_inter.go
+++ meth_inter.go
@@ -171,23 +171,25 @@
 
 // 16-26 Type switches//
 
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
-}
+// func do(i interface{}) {
+// 	switch v := i.(type) {
+// 	case int:
+// 		fmt.Printf("Twice %v is %v\n", v, v*2)
+// 	case string:
+// 		fmt.Printf("%q is %v bytes long\n", v, len(v))
+// 	default:
+// 		fmt.Printf("I don't know about type %T!\n", v)
+// 	}
+// }
 
-func main() {
-	do(21)
-	do("hello")
-	do(true)
-}
+// func main() {
+// 	do(21)
+// 	do("hello")
+// 	do(true)
+// }
+
+///
