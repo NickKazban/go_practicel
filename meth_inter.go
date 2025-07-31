b781bf77025d0bcb3d3145e3629a36df934b3656
NickKazban <mykola.kazban@gmail.com>
2025-07-31 11:40:41 +0300

    task 6-26

--- meth_inter.go
+++ meth_inter.go
@@ -71,28 +71,60 @@
 
 // 5-26 Pointers and functions//
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// type Vertex struct {
+// 	X, Y float64
+// }
+
+// func Abs(v Vertex) float64 {
+// 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// }
+
+// func Scale(v *Vertex, f float64) { // Ця функція масштабує вектор v на множник f. // Зверни увагу, що вона приймає вказівник *Vertex, тобто змінює значення v напряму.
+// 	v.X = v.X * f //Зверни увагу, що вона приймає вказівник *Vertex, тобто змінює значення v напряму.
+// 	v.Y = v.Y * f
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	Scale(&v, 10) // використання множинника для функції  Scale
+// 	fmt.Println(Abs(v))
+// }
+
+// 6-26 Methods and pointer indirection//
+
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 type Vertex struct {
 	X, Y float64
 }
 
-func Abs(v Vertex) float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
-func Scale(v *Vertex, f float64) { // Ця функція масштабує вектор v на множник f. // Зверни увагу, що вона приймає вказівник *Vertex, тобто змінює значення v напряму.
-	v.X = v.X * f //Зверни увагу, що вона приймає вказівник *Vertex, тобто змінює значення v напряму.
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
 func main() {
 	v := Vertex{3, 4}
-	Scale(&v, 10) // використання множинника для функції  Scale
-	fmt.Println(Abs(v))
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)
 }
